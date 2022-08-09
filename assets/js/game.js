2f2184acdcc88f4211a62b9bb4f742d3b8647c73
awchen <awchen85@gmail.com>
2022-08-09 16:44:43 -0400

    added function to add new, full hp robot upon defeat

--- assets/js/game.js
+++ assets/js/game.js
@@ -8,8 +8,8 @@ var enemyHealth = 50;
 var enemyAttack = 12;
 
 var fight = function(enemyName) {
+    while(enemyHealth > 0) {
     //Alert players that they are starting the round
-    window.alert("Welcome to Robot Gladiators!");
 
     var promptFight= window.prompt("Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose.");
     if (promptFight === "fight" || promptFight === "FIGHT" ) {
@@ -56,8 +56,10 @@ var fight = function(enemyName) {
         window.alert("You need to choose a valid option. Try again!");
     }
 };
-
+};
 for(var i = 0; i < enemyNames.length; i++) {
-    fight(enemyNames[i]);
+    var pickedEnemyName = enemyNames[i];
+    enemyHealth = 50;
+    fight(pickedEnemyName[i]);
 }
 

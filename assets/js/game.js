3370373c76c5f3609cb78faf686c93a5faee4d96
awchen <awchen85@gmail.com>
2022-08-09 18:30:34 -0400

    added round counter, player death message

--- assets/js/game.js
+++ assets/js/game.js
@@ -62,8 +62,22 @@ var fight = function(enemyName) {
 };
 
 for(var i = 0; i < enemyNames.length; i++) {
-    var pickedEnemyName = enemyNames[i];
-    enemyHealth = 50;
-    fight(pickedEnemyName[i]);
+    if (playerHealth > 0) {
+        window.alert("Welcome to Robot Gladiators! Round " + ( i + 1) );
+        //pick new enemy to fight based on the index of the enemyNames array
+        var pickedEnemyName = enemyNames[i];
+        //reset enemyHealth before starting new fight
+        enemyHealth = 50;
+        //use debugger to pause script from running, check what's going on
+        //debugger;
+        else {
+            window.alert("You have lost your robot in battle! Game Over!");
+            break;
+        }
+        //pass the pickedEnemyName variable's value into the fight function, where it will assume the value of the enemyName parameter
+        fight(pickedEnemyName);
+    }
+  
+  
 }
 
